1aad1d48e4e3572baae999a5b21e6283b928b456
tanxiyan <85327039+tanxiyan@users.noreply.github.com>
2024-11-14 10:25:11 +0800

    Add files via upload

---
+++ go/miniBlog/global/global.go
@@ -0,0 +1,26 @@
+// Copyright 2014 wudaoren.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+package global
+
+import (
+	"xorm.io/xorm"
+)
+
+//配置参数
+var Config struct {
+	Host  string
+	Mysql struct {
+		Host     string
+		Username string
+		Password string
+		Database string
+	}
+	Limit int
+}
+
+//数据库引擎
+var DB *xorm.Engine
+
+//上传文件保存目录
+var UPLOAD_DIR = "/static/upload/"
